c963c5b4e594a75e715a21a74f419cb2b3d4c02d
Aseem Athale <athaleaseem@gmail.com>
2021-09-30 12:17:32 +0530

    blossom: remove prop override for ro.vendor.build.security_patch
    Also remove prop override for ro.vendor.build.security_patch
    as already set by VENDOR_SECURITY_PATCH
    
    Change-Id: I3a66636547298ec044fe3f0749f0a66d7aaecc73
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- device.mk
+++ device.mk
@@ -38,7 +38,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
     ro.bootimage.build.date.utc=0 \
     ro.build.date.utc=0
 
