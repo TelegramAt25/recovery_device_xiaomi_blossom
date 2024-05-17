47d12e386a7b77d9cd3560df5ca729a2cb4d8a68
fukiame <fukiame@proton.me>
2024-05-17 15:00:11 +0700

    blossom: attempt to silence kernel messages
    miui kernel is ridiculously spammy
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=bootloader
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += quiet loglevel=3
 
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
